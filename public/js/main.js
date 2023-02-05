c36c6e819075ada4a7f94bd7751233daf3f874ee
Kartikkhode <kartikkhode21@gmail.com>
2023-02-05 13:39:33 +0530

    updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,20 @@
-document.getElementById('tosignup').addEventListener('click', ()=>{
-    // console.log('click');
-    document.querySelector('#loginform').style.display="none"
-    document.querySelector('#signupfrom').style.display="block"
-})
-document.getElementById('tologin').addEventListener('click', ()=>{
-    // console.log('click');
-    document.querySelector('#loginform').style.display="block"
-    document.querySelector('#signupfrom').style.display="none"
-})
+// document.getElementById('tosignup').addEventListener('click', ()=>{
+//     // console.log('click');
+//     document.querySelector('#loginform').style.display="none"
+//     document.querySelector('#signupfrom').style.display="block"
+// })
+// document.getElementById('tologin').addEventListener('click', ()=>{
+//     // console.log('click');
+//     document.querySelector('#loginform').style.display="block"
+//     document.querySelector('#signupfrom').style.display="none"
+// })
 
  const cart = document.querySelector('.cart-content');
+ const card = document.querySelector('.card');
 
- document.getElementsByClassName('tocart').addEventListener('click', ()=>{
-  console.log('click');
- })
+//  document.getElementById('tocart(id)').addEventListener('click', ()=>{
+//   console.log('click');
+//  })
 // console.log(a);
 //  if (cart) 
 //  cart.addEventListener('submit', e => {
@@ -26,12 +27,27 @@ document.getElementById('tologin').addEventListener('click', ()=>{
 
 
 var toadd = document.getElementsByClassName("tocart");
-        console.log(toadd);
+
         for(var i=0 ; i < toadd.length; i++){
             var button = toadd[i];
             console.log(button)
-        // var title = document.getElementsByClassName("title")[i].innerText;
-        // console.log(title);
-        button.addEventListener("click", clicked)
-        // i++;
+            button.addEventListener('click', clicked);
+            //     var button = event.target;
+            //     var cartProducts = button.parentElement;
+            //     console.log('clicked');
+            //     var title = cartProducts.getElementsByClassName("title").innerText;
+            //     var price = cartProducts.getElementById("itemprice").innerText;
+            //     console.log(title);
+            //     console.log(price);
+            // })
+    }
+    // console.log(toadd);
+    function clicked(event){
+            var button = event.target;
+            var cartProducts = button.parentElement;
+            console.log(cartProducts);
+            var title = cartProducts.getElementsByClassName('title').innerText;
+            console.log(title);
+            var price = cartProducts.getElementsByClassName('itemprice').value;
+            console.log(price);
         }
