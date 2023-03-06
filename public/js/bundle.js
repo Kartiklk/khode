e6295d6de0844aabc2828febcdf2bbf479c7290f
Kartikkhode <kartikkhode21@gmail.com>
2023-03-06 16:17:54 +0530

    updated

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12400,7 +12400,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50270" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58180" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -7,7 +7,7 @@ router.get('/' , authController.isLoggedIn, viewController.overview);
 router.get('/itemslist',authController.isLoggedIn, viewController.itemList);
 router.get('/items/:id', authController.isLoggedIn, viewController.item);
 router.get('/login', authController.isLoggedIn, viewController.loginform);
-router.get('/cart',authController.protect, viewController.getMycart);
+router.get('/cart',authController.isLoggedIn, authController.protect, viewController.getMycart);
 router.get('/order',authController.protect, viewController.Orderdetails);
 
 module.exports = router;
