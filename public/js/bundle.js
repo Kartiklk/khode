e9089898d252ca693a1e5f0b13cc01efef61dd3c
Kartikkhode <kartikkhode21@gmail.com>
2023-04-21 20:53:36 +0530

    updated

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -69718,7 +69718,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64036" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58023" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
